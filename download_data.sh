93fa04551f4a3613d5c5c8d7dfb8b216b4cb6abf
Shane <shanemcq18@gmail.com>
2017-03-03 21:45:34 -0700

    simplify download data

--- download_data.sh
+++ download_data.sh
@@ -34,23 +34,23 @@ git pull origin master
 # Copy the files over from the temporary folder.
 cd $START
 
-mv $PYTHONESSENTIALS/grid.npy NumpyIntro/grid.npy
-mv $PYTHONESSENTIALS/FARS.npy MatplotlibIntro/FARS.npy
-mv $PYTHONESSENTIALS/MLB.npy DataVisualization/MLB.npy
-mv $PYTHONESSENTIALS/anscombe.npy DataVisualization/anscombe.npy
-mv $PYTHONESSENTIALS/countries.npy DataVisualization/countries.npy
-mv $PYTHONESSENTIALS/earthquakes.npy DataVisualization/earthquakes.npy
+mv $PYTHONESSENTIALS/grid.npy NumpyIntro/
+mv $PYTHONESSENTIALS/FARS.npy MatplotlibIntro/
+mv $PYTHONESSENTIALS/MLB.npy DataVisualization/
+mv $PYTHONESSENTIALS/anscombe.npy DataVisualization/
+mv $PYTHONESSENTIALS/countries.npy DataVisualization/
+mv $PYTHONESSENTIALS/earthquakes.npy DataVisualization/
 
-mv $VOLUME1/pi.npy LinearTransformations/pi.npy
-mv $VOLUME1/horse.npy LinearTransformations/horse.npy
-mv $VOLUME1/circle.npy LeastSquares_Eigenvalues/circle.npy
-mv $VOLUME1/ellipse.npy LeastSquares_Eigenvalues/ellipse.npy
-mv $VOLUME1/housing.npy LeastSquares_Eigenvalues/housing.npy
-mv $VOLUME1/dream.png ImageSegmentation/dream.png
-mv $VOLUME1/hubble_image.jpg SVD_ImageCompression/hubble_image.jpg
-# mv $VOLUME1/cameraman.jpg NumericalDifferentiation/cameraman.jpg
-mv $VOLUME1/matrix.txt PageRank/matrix.txt
-mv $VOLUME1/ncaa2013.csv PageRank/ncaa2013.csv
+mv $VOLUME1/pi.npy LinearTransformations/
+mv $VOLUME1/horse.npy LinearTransformations/
+mv $VOLUME1/circle.npy LeastSquares_Eigenvalues/
+mv $VOLUME1/ellipse.npy LeastSquares_Eigenvalues/
+mv $VOLUME1/housing.npy LeastSquares_Eigenvalues/
+mv $VOLUME1/dream.png ImageSegmentation/
+mv $VOLUME1/hubble_image.jpg SVD_ImageCompression/
+# mv $VOLUME1/cameraman.jpg NumericalDifferentiation/
+mv $VOLUME1/matrix.txt PageRank/
+mv $VOLUME1/ncaa2013.csv PageRank/
 
 # Delete the temporary folder and return to the original folder.
 cd ../
