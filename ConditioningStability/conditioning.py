dde4a016a5afa02a14f98ea70631a0d71c4ca24e
Shane <shanemcq18@gmail.com>
2017-03-18 22:59:21 -0600

    march 2017 update

--- ConditioningStability/conditioning.py
+++ ConditioningStability/conditioning.py
@@ -7,8 +7,22 @@
 
 import numpy as np
 
+
 # Problem 1
-def prob1():
+def prob1(A):
+    """Calculate the condition number of a matrix using the 2-norm for matrices.
+    
+    Parameters:
+        A ((m,n) ndarray): A matrix.
+    
+    Returns:
+        x (float): The condition number of the matrix.
+    """
+    raise NotImplementedError("prob1 incomplete")
+
+    
+# Problem 2
+def prob2():
     """Randomly perturb w_coeff by replacing each coefficient a_i with
     a_i*r_i, where r_i is drawn from a normal distribution centered at 1 with
     standard deviation 1e-10.
@@ -16,72 +30,84 @@ def prob1():
     Plot the roots of 100 such experiments in a single graphic, along with the
     roots of the unperturbed polynomial w(x).
 
-    Using the final experiment only, estimate the relative and absolute
-    condition number (in any norm you prefer).
+    Using the final experiment only, using the infinity norm, estimate the
+    relative and absolute condition number.
 
     Returns:
         Display a graph of all 100 perturbations.
-        Print the values of relative and absolute condition numbers.
+        tuple (float): A tuple containing the average absolute and relative 
+            condition numbers
     """
-    w_roots = np.arange(1, 21)
-    w_coeffs = np.array([1, -210, 20615, -1256850, 53327946, -1672280820,
-                        40171771630, -756111184500, 11310276995381,
-                        -135585182899530, 1307535010540395,
-                        -10142299865511450, 63030812099294896,
-                        -311333643161390640, 1206647803780373360,
-                        -3599979517947607200, 8037811822645051776,
-                        -12870931245150988800, 13803759753640704000,
-                        -8752948036761600000, 2432902008176640000])
-    raise NotImplementedError("Problem 1 Incomplete")
+    raise NotImplementedError("prob2 incomplete")
 
 
-# Problem 2
+# Problem 3
 def eig_condit(M):
     """Approximate the condition number of the eigenvalue problem at M.
 
-    Inputs:
+    Parameters:
         M ((n,n) ndarray): A square matrix.
 
     Returns:
         (float) absolute condition number of the eigenvalue problem at M.
         (float) relative condition number of the eigenvalue problem at M.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    raise NotImplementedError("eig_condit incomplete")
 
-def plot_eig_condit(x0=-100, x1=100, y0=-100, y1=100, res=10):
+# Problem 4
+def prob4(x0=-100, x1=100, y0=-100, y1=100, res=10):
     """Create a grid [x0, x1] x [y0, y1] with the given resolution. For each
     entry (x,y) in the grid, find the relative condition number of the
-    eigenvalue problem, using the matrix   [[1 x]
-                                            [y 1]]  as the input.
+    eigenvalue problem, using the matrix   [[1, x], [y, 1]]  as the input.
     Use plt.pcolormesh() to plot the condition number over the entire grid.
 
-    Inputs:
+    Parameters:
         x0 (float): min x-value.
         x1 (float): max x-value.
         y0 (float): min y-value.
         y1 (float): max y-value.
         res (int): number of points along each edge of the grid.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
-
-
-# Problem 3
-def integral(n):
-    """Calculate the integral from 0 to 1 of x^n e^{x-1} dx using the closed
-    form solution (-1)^n !n + (-1)^{n+1} n!/e.
+    raise NotImplementedError("prob4 incomplete")
+    
+    
+# Problem 5
+def lstsq_normal(A, b):
+    """Calculate the least squares solution of Ax=b directly using the normal
+    equation
+    
+    Inputs:
+        A ((m,n), ndarray): A matrix of size m>=n
+        b ((m, ), ndarray): A vector of length m
+        
+    Returns:
+        x ((m, ), ndarray): The vector solution to the normal equation
     """
-    raise NotImplementedError("Problem 3 Incomplete")
-
-def prob3():
-    """For the values of n in the problem, compute integral(n). Compare
-    the values to the actual values, and print your explanation of what
-    is happening.
+    raise NotImplementedError("lstsq_normal incomplete")
+    
+    
+def lstsq_qr(A, b):
+    """Calculate the least squares solution of Ax=b using the QR decomposition.
+    
+    Inputs:
+        A ((m,n), ndarray): A matrix of size m>=n
+        b ((m, ), ndarray): A vector of length m
+        
+    Returns:
+        x ((m, ), ndarray): The vector solution to the normal equation
     """
-    # Actual values of the integral at specified n.
-    actual_values = {    1: 0.367879441171,   5: 0.145532940573,
-                        10: 0.0838770701034, 15: 0.0590175408793,
-                        20: 0.0455448840758, 25: 0.0370862144237,
-                        30: 0.0312796739322, 35: 0.0270462894091,
-                        40: 0.023822728669,  45: 0.0212860390856,
-                        50: 0.0192377544343                         }
-    raise NotImplementedError("Problem 3 Incomplete")
+    raise NotImplementedError("lstsq_qr incomplete")
+    
+    
+def prob5():
+    """Approximate e^sin(4x) on the interval [0,1] with a 14 degree polynomial using
+    the least squares method. Solve the least squares problem using the the normal 
+    equation and the QR decomposition and then compare the solutions by plotting
+    them together with the interpolated function.  Return the mean squared error of
+    both solutions, ||Ax-b||_2.
+    
+    Returns:
+        error1 (float): The mean squared error of the direct approach
+        error2 (float): The mean squared error using the QR decomposition
+    """
+    raise NotImplementedError("prob5 incomplete")
--- SymbolicAutomaticDifferentiation/sympy_autograd.py
+++ SymbolicAutomaticDifferentiation/sympy_autograd.py
@@ -4,91 +4,87 @@
 <Class>
 <Date>
 """
-
+import sympy as sy
+import numpy as np
+import autograd.numpy as anp
 
 # Problem 1
-def myexp(n):
-    """Compute e to the nth digit.
-
-    Inputs:
-        n (integer): n decimal places to calculate e.
-
-    Returns:
-        approximation (float): approximation of e.
-    """
-    raise NotImplementedError("Problem 1 Incomplete")
-
+def prob1():
+	""" Write out the expression 2/5*e^(x^2-y)cosh(x+y) + 3/7*log(xy+1) in SymPy.
+	
+	Returns:
+	 The SymPy Expression 
+	"""
+	raise NotImplementedError("Problem 1 Incomplete")
 
 # Problem 2
 def prob2():
-    """Solve y = e^x + x for x.
-
+    """Solve y = sqrt(5-e^x^2) for x.
+    
     Returns:
-        the solution (list).
+        (list): The list of expressions that solves for x.
     """
     raise NotImplementedError("Problem 2 Incomplete")
 
 
 # Problem 3
 def prob3():
-    """Compute the integral of sin(x^2) from 0 to infinity.
-
-    Returns:
-        the integral value (float).
-    """
-    raise NotImplementedError("Problem 3 Incomplete")
-
-
-# Problem 4
-def prob4():
     """Calculate the derivative of e^sin(cos(x)) at x = 1.
-    Time how long it takes to compute the derivative using SymPy as well as
-    centered difference quotients.
-    Calculate the error for each approximation.
+    Time how long it takes to compute the derivative using SymPy.
+    Time the same derivative using the second order centered difference quotient.
+	Calculate the error for each approximation.
 
     Print the time it takes to compute and the error for both SymPy and
     centered difference quotients.
 
     Returns:
-        SymPy approximation (float)
+        (float): SymPy approximation of e^sin(cos(x)) at x = 1.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+
+    raise NotImplementedError("Problem 3 Incomplete")
+
+#Problem 4
+def prob4():
+	"""Evaluate the 3rd derivative of tanh(x) at 10,000 randomly generated points
+	using .subs() method and the lambdify() function. 
+	
+	Print the time it takes to compute the 3rd derivative of tanh(x) of these 
+	two methods. 
+	"""
+	raise NotImplementedError("Problem 3 Incomplete")
 
 
 # Problem 5
 def prob5():
-    """Solve the differential equation when x = 1.
+    """Compute the derivative of ln(sqrt(sin(sqrt(x)))) at x = pi/4.
+    Times how long it take to compute using SymPy, Autograd, and centered
+    difference quotients. Compute the error of each approximation.
 
-    Returns:
-        Solution when x = 1.
+    Print the time and error for each computation. 
+    
     """
     raise NotImplementedError("Problem 5 Incomplete")
 
-
 # Problem 6
 def prob6():
-    """Compute the derivative of ln(sqrt(sin(sqrt(x)))) at x = pi/4.
-    Times how long it take to compute using SymPy, autograd, and centered
-    difference quotients. Compute the error of each approximation.
-
-    Print the time
-    Print the error
-
-    Returns:
-        derviative (float): the derivative computed using autograd.
-    """
-    raise NotImplementedError("Problem 6 Incomplete")
-
+	""" Use elementwise_grad() from autograd to create the following functions: f(x) = 1/cosh(x) 
+	and its next 5 derivatives df(x),d2f(x),d3f(x),d4f(x), and d5f(x) from [-7,7]. 
+		
+	Plot each function in multiple subplots.
+	
+	"""
+	raise NotImplementedError("Problem 6 Incomplete")
 
 # Problem 7
 def prob7():
-    """Computes Jacobian for the function
+    """Compute Jacobian for the function
         f(x,y)=[(e^x)sin(y) + y^3, 3y - cos(x)]
     Time how long it takes to compute the Jacobian using SymPy and autograd.
 
-    Print the times.
+    Print the computation times.
 
     Returns:
-        Jacobian (array): jacobian found using autograd at (x,y) = (1,1)
+        Jacobian (array): jacobian found using Autograd at (1,1).
     """
+    
     raise NotImplementedError("Problem 7 Incomplete")
